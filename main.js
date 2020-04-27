7bacfc63ee4376d857aa59a9ab759d7034eae165
PoltavetsY <Poltavets.Y@gmail.com>
2020-04-27 23:02:47 +0300

    range block changed

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var range = document.getElementById('range');
 var number = document.getElementById('number');
+var commission = document.getElementById('red');
+var greenBlock = document.getElementById('green');
 
 setInterval (function(){
     number.value = range.value;
-  }, 0);
+    greenBlock.style.height = number.value + 'px';
+    // commission.style.height = (number.value * 10 / 100) + 'px'; 
+}, 0);
+
+
 
