f159b641b68469e07460b401c6982ae2130ce084
PoltavetsY <Poltavets.Y@gmail.com>
2020-05-29 11:12:31 +0300

    optimized version

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 var range = document.getElementById('range');
 var number = document.getElementById('number');
-var commission = document.getElementById('red');
+var commissionBlock = document.getElementById('red');
 var greenBlock = document.getElementById('green');
 
-setInterval (function(){
-    number.value = range.value;
-    greenBlock.style.height = number.value + 'px';
-    // commission.style.height = (number.value * 10 / 100) + 'px'; 
-}, 0);
+var calcCommission = function ({commissionBlock, number}) {
+    if (range.value <= 20) {
+        return commissionBlock.style.height =  (number.value * 0.02) + 'px'; 
+    } else if (range.value > 20 && range.value < 50) {
+        return commissionBlock.style.height =  (number.value * 0.04) + 'px';
+    } else if (range.value >= 75) {
+        return commissionBlock.style.height =  (number.value * 0.08) + 'px'; 
+    } else {
+        return commissionBlock.style.height =  (number.value * 0.06) + 'px'; 
+    } 
+}.bind(null, {commissionBlock, number});
 
+var actions = {
+    number, 
+    calcCommission,
+    getRangeValue() {
+        return range.value = this.number.value;
+    },
+    getGreenBlockHeight() {
+        return greenBlock.style.height = this.number.value + 'px';
+    }
+};
 
+range.addEventListener('mousemove', function() {
+    number.value = this.value;
+});
+range.addEventListener('mousemove', actions.getGreenBlockHeight.bind(actions));
+range.addEventListener('mousemove', actions.calcCommission);
 
+number.addEventListener('input', actions.getRangeValue.bind(actions));
+number.addEventListener('input', actions.getGreenBlockHeight.bind(actions));
+number.addEventListener('input', actions.calcCommission);
